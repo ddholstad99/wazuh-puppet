4a450dfdb80ad46286f441633e02acf1f9ccf974
Dan Holstad <dan-holstad@uiowa.edu>
2025-07-07 14:13:16 -0500

    switched to systemd for rocky8

--- manifests/params_agent.pp
+++ manifests/params_agent.pp
@@ -460,7 +460,7 @@ class wazuh::params_agent {
             }
             'Rocky': {
               if ( $::operatingsystemrelease =~ /^8.*/ ) {
-                $ossec_service_provider = 'redhat'
+                $ossec_service_provider = 'systemd'
               }
             }
             default: { fail('This ossec module has not been tested on your distribution') }
